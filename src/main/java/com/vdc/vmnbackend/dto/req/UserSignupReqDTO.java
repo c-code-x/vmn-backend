828df46dbe0c34ae31e182d10f8220681d5aec64
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2024-02-10 11:49:36 +0530

    Update UserSignupReqDTO.java

--- src/main/java/com/vdc/vmnbackend/dto/req/UserSignupReqDTO.java
+++ src/main/java/com/vdc/vmnbackend/dto/req/UserSignupReqDTO.java
@@ -1,32 +1,58 @@
 package com.vdc.vmnbackend.dto.req;
 
-import com.vdc.vmnbackend.dao.UserDAO;
 import com.vdc.vmnbackend.enumerators.Roles;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
-
+/**
+ * Represents a request DTO for user sign-up with required user details.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
 public record UserSignupReqDTO(
-
+        /**
+         * The name of the user signing up.
+         */
         @NotEmpty
         String name,
+
+        /**
+         * The email address of the user signing up.
+         */
         @NotEmpty
         @Email
         String emailId,
+
+        /**
+         * The password of the user signing up.
+         */
         @NotEmpty
         String password,
+
+        /**
+         * The role of the user signing up.
+         */
         @NotEmpty
         Roles role,
+
+        /**
+         * The contact information of the user signing up.
+         */
         @NotEmpty
         String contact,
+
+        /**
+         * The LinkedIn profile link of the user signing up.
+         */
         @NotEmpty
         String linkedIn,
+
+        /**
+         * The designation or job title of the user signing up.
+         */
         @NotEmpty
         String designation
 ) {
-
 }
