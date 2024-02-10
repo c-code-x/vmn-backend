4f05e874912cc7bbef28d9baee1b7e264a50cff2
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2024-02-10 11:42:30 +0530

    Update AdminDAO.java

--- src/main/java/com/vdc/vmnbackend/dao/AdminDAO.java
+++ src/main/java/com/vdc/vmnbackend/dao/AdminDAO.java
@@ -3,10 +3,12 @@ package com.vdc.vmnbackend.dao;
 import com.vdc.vmnbackend.enumerators.Campus;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
+/**
+ * Represents an admin entity in the system.
+ */
 @Entity
 @Builder
 @Getter
@@ -16,23 +18,38 @@ import java.util.UUID;
 @ToString
 @Table(name = "admin")
 public class AdminDAO {
+    /**
+     * The unique identifier for the admin entity.
+     */
     @Id
     @GeneratedValue(generator = "UUID")
     @UuidGenerator
     private UUID dataId;
+
+    /**
+     * The user associated with this admin.
+     */
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "adminId", referencedColumnName = "uid", nullable = false, updatable = false)
     @MapsId
     private UserDAO adminId;
 
-
+    /**
+     * The user who created this admin entity.
+     */
     @ManyToOne
-    @JoinColumn(name = "createdBy", referencedColumnName = "uid",nullable = false)
+    @JoinColumn(name = "createdBy", referencedColumnName = "uid", nullable = false)
     private UserDAO createdBy;
 
+    /**
+     * The campus where the admin is located.
+     */
     @Column(name = "campus")
     private Campus campus;
 
+    /**
+     * The biography of the admin.
+     */
     @Column(name = "bio")
     @Builder.Default
     private String bio = "I am an admin, This is My Bio!";
