ada68cf3a80d876dbed2c79acea4bfc112ac9252
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2024-02-10 11:58:52 +0530

    Update ApiRuntimeException.java

--- src/main/java/com/vdc/vmnbackend/exception/ApiRuntimeException.java
+++ src/main/java/com/vdc/vmnbackend/exception/ApiRuntimeException.java
@@ -3,13 +3,21 @@ package com.vdc.vmnbackend.exception;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Custom runtime exception for API errors.
+ */
 @Getter
 @Setter
 public class ApiRuntimeException extends RuntimeException {
     private final HttpStatus httpStatus;
 
+    /**
+     * Constructs a new ApiRuntimeException with the specified error message and HTTP status.
+     *
+     * @param message    The error message.
+     * @param httpStatus The HTTP status associated with the error.
+     */
     public ApiRuntimeException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
