30203803ab87e1c4795495085bfb2ed7774fac68
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2024-02-10 11:49:04 +0530

    Update UserSigninReqDTO.java

--- src/main/java/com/vdc/vmnbackend/dto/req/UserSigninReqDTO.java
+++ src/main/java/com/vdc/vmnbackend/dto/req/UserSigninReqDTO.java
@@ -3,12 +3,21 @@ package com.vdc.vmnbackend.dto.req;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
-import lombok.NonNull;
 
-
+/**
+ * Represents a request DTO for user sign-in with provided credentials.
+ */
+@Data
 public record UserSigninReqDTO(
+        /**
+         * The email address of the user signing in.
+         */
         @Email
         String emailId,
+
+        /**
+         * The password of the user signing in.
+         */
         @NotEmpty
         String password
 ) {
