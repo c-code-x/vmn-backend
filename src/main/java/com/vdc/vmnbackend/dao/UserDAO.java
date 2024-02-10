e528a8563d794b6111b9c37650034750e051dee0
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2024-02-10 11:45:50 +0530

    Update UserDAO.java

--- src/main/java/com/vdc/vmnbackend/dao/UserDAO.java
+++ src/main/java/com/vdc/vmnbackend/dao/UserDAO.java
@@ -7,6 +7,9 @@ import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
+/**
+ * Represents a user entity in the system.
+ */
 @Entity
 @Getter
 @Setter
@@ -16,34 +19,56 @@ import java.util.UUID;
 @Table(name = "users")
 @Builder
 public class UserDAO {
+    /**
+     * The unique identifier for the user.
+     */
     @Id
     @GeneratedValue(generator = "UUID")
     @UuidGenerator
     @Column(name = "uid", unique = true, updatable = false, nullable = false)
     private UUID uid;
 
-    @Column(name = "name",nullable = false)
+    /**
+     * The name of the user.
+     */
+    @Column(name = "name", nullable = false)
     private String name;
 
+    /**
+     * The email ID of the user.
+     */
     @Column(name = "emailId", nullable = false, unique = true)
     private String emailId;
 
+    /**
+     * The role of the user.
+     */
     @Column(name = "role", nullable = false)
     @Builder.Default
     @Enumerated(EnumType.STRING)
     private Roles role = Roles.USER;
 
-    @Column(name = "password",nullable = false)
+    /**
+     * The password of the user.
+     */
+    @Column(name = "password", nullable = false)
     private String password;
 
+    /**
+     * The contact number of the user.
+     */
     @Column
     private String contact;
 
+    /**
+     * The LinkedIn profile of the user.
+     */
     @Column
     private String linkedIn;
 
+    /**
+     * The designation of the user.
+     */
     @Column
     private String designation;
-
-
 }
